1964eac4afac718035b73068e3271b7eb1f5de1d
Le Anh Khoa <khoale22082004@gmail.com>
2026-05-02 20:06:10 +0700

    fixed alt imge thumbnail

--- code-master-ai-fe/src/pages/myCourses/index.tsx
+++ code-master-ai-fe/src/pages/myCourses/index.tsx
@@ -95,7 +95,7 @@ const MyCourses: React.FC = () => {
               <div className="aspect-video overflow-hidden relative">
                 <img
                   src={course.thumbnail}
-                  // alt={course.title}
+                  alt=""
                   className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
                 />
                 <div className="absolute top-3 left-3">
